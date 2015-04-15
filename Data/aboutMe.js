238ef489ae1b35fedfddbdc8477fcb99b8e7a19a
Sourav Dhar <souravkrdhar@gmail.com>
2015-04-15 13:57:13 +0530

    Portfolio

--- Data/aboutMe.js
+++ Data/aboutMe.js
@@ -1,3 +1,3 @@
 var me ={
-    skills:['AngularJS', 'HTML5', 'CSS3', 'ChromeDevTools', 'Javascript', 'Jquery', 'Bootstrap', 'ResponsiveDesign', 'ASP.NET', 'JAVA', 'Photoshop', 'Arduino']
+    skills:['AngularJS', 'HTML5', 'CSS3', 'ChromeDevTools', 'Javascript', 'Jquery', 'Bootstrap', 'ResponsiveDesign', 'ASP.NET', 'C#','MS-SQL','ANSI-SQL', 'JAVA', 'Photoshop', 'Arduino']
 };
