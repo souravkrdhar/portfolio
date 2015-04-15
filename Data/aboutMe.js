43d1c36c67d7457862993730fa64f4fc16dd68b3
Sourav Dhar <souravkrdhar@gmail.com>
2015-04-15 23:26:49 +0530

    Portfolio

--- Data/aboutMe.js
+++ Data/aboutMe.js
@@ -1,3 +1,7 @@
 var me ={
-    skills:['AngularJS', 'HTML5', 'CSS3', 'ChromeDevTools', 'Javascript', 'Jquery', 'Bootstrap', 'ResponsiveDesign', 'ASP.NET', 'C#','MS-SQL','ANSI-SQL', 'JAVA', 'Photoshop', 'Arduino']
+    education:[{Degree: 'Master of Technology', University: 'Jadavpur University', Stream:'Distributed & Mobile Computing'},
+                {Degree:'Bachelor of Technology', University: 'WBUT', Stream: 'Information Technology'},
+                {Degree:'High School', University:'Indian School Certificate', Stream:'The Future Foundation School'}],
+    contact:[{mail:'souravkrdhar@gmail.com',phone:'+91-990-35-96315'}],
+    skills:['AngularJS', 'HTML5', 'CSS3', 'Chrome Developer Tools', 'Javascript', 'Jquery', 'Bootstrap', 'Responsive Design', 'ASP.NET', 'C#','Java', 'Object Oriented Programming','MS-SQL','ANSI-SQL', 'JAVA', 'Photoshop', 'Arduino']
 };
