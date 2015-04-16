2b7e4853dda24ef60fadc65e2dee41bc2194f78c
Sourav Dhar <souravkrdhar@gmail.com>
2015-04-16 17:50:53 +0530

    Portfolio

--- Data/aboutMe.js
+++ Data/aboutMe.js
@@ -1,7 +1,9 @@
 var me ={
-    education:[{Degree: 'Master of Technology', University: 'Jadavpur University', Stream:'Distributed & Mobile Computing'},
-                {Degree:'Bachelor of Technology', University: 'WBUT', Stream: 'Information Technology'},
-                {Degree:'High School', University:'Indian School Certificate', Stream:'The Future Foundation School'}],
-    contact:[{mail:'souravkrdhar@gmail.com',phone:'+91-990-35-96315'}],
-    skills:['AngularJS', 'HTML5', 'CSS3', 'Chrome Developer Tools', 'Javascript', 'Jquery', 'Bootstrap', 'Responsive Design', 'ASP.NET', 'C#','Java', 'Object Oriented Programming','MS-SQL','ANSI-SQL', 'JAVA', 'Photoshop', 'Arduino']
+    workExperience:[{Company:'Cognizant', Start:'Nov, 4th 2014', End:'Present'}],
+    hobbies:['Programming', 'Football', 'Computer Games', 'Music'],
+    education:[{Degree: 'Master of Technology', Place: 'Jadavpur University', Course:'Distributed & Mobile Computing'},
+                {Degree:'Bachelor of Technology', Place: 'WBUT', Course: 'Information Technology'},
+                {Degree:'Indian School Certificate', Place:'The Future Foundation School', Course:'High School'}],
+    contact:{mail:'souravkrdhar@gmail.com',phone:['+91-990-35-96315']},
+    skills:['AngularJS', 'HTML5', 'CSS3', 'Chrome Developer Tools', 'Javascript', 'Jquery', 'Bootstrap', 'Responsive Design', 'ASP.NET', 'C#','Java', 'Object Oriented Programming','MS-SQL','ANSI-SQL', 'Swing', 'Photoshop', 'Arduino']
 };
